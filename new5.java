39485bd8c0d0d2acb22a4ff0d5e5dce347cadead
hackershub11 <72488167+hackershub11@users.noreply.github.com>
2025-10-27 07:14:05 +0530

    Create new5.java

---
+++ new5.java
@@ -0,0 +1,16 @@
+// Java program to demonstrate 
+// Boolean toString() method 
+class Geeks
+ { 
+	public static void main(String[] args) 
+	{ 
+		// boolean type value 
+		boolean value = true; 
+
+		// static toString() method of Boolean class 
+		String output = Boolean.toString(value); 
+
+		// printing the value 
+		System.out.println(output); 
+	} 
+}
